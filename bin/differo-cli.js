463cd50742629b7eeee2ee17285b80b34e9d3c09
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2017-11-12 00:23:39 +0100

    Glue together CLI/ImageWriter/Differo/Screenshot

--- bin/differo-cli.js
+++ bin/differo-cli.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 
 const { CLI } = require('../lib/cli.js');
+const { Differo } = require('../lib/differo.js');
+const { Browser } = require('../lib/browser.js');
+
+const browser = new Browser();
+const differo = new Differo({ browser });
 
 const cli = new CLI({
   write: console.log,
   argv: process.argv,
+  differo,
+  browser,
 });
 
 cli.run();
