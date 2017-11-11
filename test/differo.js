6b36cb4d0269fefcb1c9e29fe62f87c7fa47dc5d
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2017-11-11 23:17:48 +0100

    refactory differo tests with sinon

--- test/differo.js
+++ test/differo.js
@@ -1,19 +1,21 @@
 const { Differo } = require('../lib/differo.js');
 const { expect } = require('chai');
+const sinon = require('sinon');
 
-const { Browser } = require('../lib/browser.js'); // FIXME Mock
+const Browser = function () {
+  this.load = sinon.spy();
+  this.captureScreenshot = sinon.spy();
+};
 
 describe('Differo', async () => {
-  const browser = new Browser({ host: 'localhost', port: 9222 });
-
-  before(() => browser.connect()); // FIXME mock should not need it
-  after(() => browser.close()); // FIXME mock should not need it
-
+  const browser = new Browser();
   const differo = new Differo({ browser });
 
-  it('Should take a screenshot, gived an URL, return a PNG buffer', async () => {
-    const result = await differo.screenshot({ 'url': 'http://localhost' });
-    expect(result).to.be.an.instanceof(Buffer);
-    expect(result.slice(1, 4).toString()).to.equal('PNG');
+  it('Should take a screenshot using browser captureScreenshot', async () => {
+    await differo.screenshot({ 'url': 'http://localhost' });
+    expect(browser.load.calledOnce).to.be.true;
+    expect(browser.load.getCall(0).args).to.eql(['http://localhost']);
+    expect(browser.captureScreenshot.calledOnce).to.be.true;
+    expect(browser.captureScreenshot.getCall(0).args).to.eql([]);
   });
 });
