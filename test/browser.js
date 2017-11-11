bffc66325ea853d93b209519e08c327d02e436fe
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2017-11-11 01:04:19 +0100

    browser connect

--- test/browser.js
+++ test/browser.js
@@ -4,11 +4,29 @@ const { expect } = require('chai');
 describe('Browser', () => {
   it('Should connect', async () => {
     const browser = new Browser({
-      hostname: 'localhost',
+      host: 'localhost',
       port: 9222,
     });
 
-    const ok = await browser.ready();
-    expect(ok).to.be.true;
+    /* This should not throw */
+    await browser.connect();
+
+    await browser.close();
+  });
+
+  it('Should throw exception if cannot connect', async () => {
+    const browser = new Browser({
+      host: 'localhost',
+      port: 9221,
+    });
+
+    try {
+      await browser.connect();
+    } catch (e) {
+      expect(e).to.be.an.instanceof(Error);
+      return;
+    }
+
+    throw new Error("Should have thrown");
   });
 });
