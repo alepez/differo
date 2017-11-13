f1feb340692b4aa89fca492b19b3f870fde95c65
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2017-11-13 21:17:12 +0100

    wait until spawned browser ready to connect

--- bin/differo-cli.js
+++ bin/differo-cli.js
@@ -4,16 +4,19 @@ const { CLI } = require('../lib/cli.js');
 const { Differo } = require('../lib/differo.js');
 const { Browser } = require('../lib/browser.js');
 const { Comparator } = require('../lib/comparator.js');
+const { Chrome } = require('../lib/chrome.js');
 
 const comparator = new Comparator();
 const browser = new Browser();
 const differo = new Differo({ browser, comparator });
+const chrome = new Chrome();
 
 const cli = new CLI({
   write: console.log,
   argv: process.argv,
   differo,
   browser,
+  chrome,
 });
 
 cli.run();
